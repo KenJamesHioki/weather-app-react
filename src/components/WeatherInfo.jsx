f54cb16c22c990d97d9f2e33ce2abc523718a81d
KenJamesHioki <kenj.hioki@gmail.com>
2024-05-02 12:37:43 +0900

    [clean]Resultコンポーネント内の分岐をなくした
    Resultコンポーネント内で複数の分岐があったが、childrenとpropsで渡すものを変更しそれを修正

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -1,26 +1,9 @@
-export const WeatherInfo = ({ temperature, weatherIcon, weather, windSpeed, children }) => {
+export const WeatherInfo = ({ title, children }) => {
   return (
     <div className="weather-info">
-      <div className="weather-info-title">{children}</div>
+      <div className="weather-info-title">{title}</div>
       <div className="weather-info-data">
-        {temperature && (
-          <p className="weather-info-main">{temperature}℃</p>
-        )}
-        {weather && (
-          <>
-            <div className="weather-info-icon-container">
-              <img
-                className="weather-info-icon"
-                src={`https://openweathermap.org/img/wn/${weatherIcon}@2x.png`}
-                alt="Weather Icon"
-              />
-            </div>
-            <p className="weather-info-sub">{weather}</p>
-          </>
-        )}
-        {windSpeed && (
-          <p className="weather-info-main">{windSpeed}m/s</p>
-        )}
+        {children}
       </div>
     </div>
   );
